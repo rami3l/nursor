2a9ac76961a2521a96871750e253f73baa37b3b4
rami3l <rami3l@outlook.com>
2024-12-26 17:12:14 +0800

    feat: impl simple LLM-based response

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject nursor "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.github.cdimascio/dotenv-java "3.1.0"]
+                 [net.clojars.wkok/openai-clojure "0.22.0"]]
   :main ^:skip-aot nursor.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/nursor/core.clj
+++ src/nursor/core.clj
@@ -1,7 +1,30 @@
 (ns nursor.core
+  (:require [wkok.openai-clojure.api :as openai])
+  (:import [io.github.cdimascio.dotenv Dotenv])
   (:gen-class))
 
+(def env (-> Dotenv (. configure) (.ignoreIfMissing) (.load)))
+
+(defrecord LLMModel [name api-key api-endpoint])
+
+(defn- llm-model-from-env []
+  (->> ["OPENAI_MODEL_NAME" "OPENAI_API_KEY" "OPENAI_API_ENDPOINT"]
+       (map #(.get env %))
+       (apply ->LLMModel)))
+
+(defn- llm-respond [model messages]
+  (openai/create-chat-completion
+   {:model (:name model)
+    :max_tokens 1024
+    :temperature 0.85
+    :messages messages}
+   (select-keys model [:api-key :api-endpoint])))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& _args]
+  (println
+   (-> (llm-model-from-env)
+       (llm-respond
+        [{:role "user"
+          :content "The Los Angeles Dodgers won the World Series in 2020. Where was it played?"}]))))
+
