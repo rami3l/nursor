ec6ee0ad84705b98d27c735f440c84ff4f789162
rami3l <rami3l@outlook.com>
2025-01-07 18:09:59 +0800

    feat: impl simple demo for next move prediction

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject nursor "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.github.cdimascio/dotenv-java "3.1.0"]
-                 [net.clojars.wkok/openai-clojure "0.22.0"]]
+                 [net.clojars.wkok/openai-clojure "0.22.0"]
+                 [io.github.java-diff-utils/java-diff-utils "4.15"]]
   :main ^:skip-aot nursor.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/nursor/core.clj
+++ src/nursor/core.clj
@@ -1,6 +1,9 @@
 (ns nursor.core
-  (:require [wkok.openai-clojure.api :as openai])
-  (:import [io.github.cdimascio.dotenv Dotenv])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [wkok.openai-clojure.api :as openai])
+  (:import [com.github.difflib DiffUtils UnifiedDiffUtils]
+           [io.github.cdimascio.dotenv Dotenv])
   (:gen-class))
 
 (def env (-> Dotenv (. configure) (.ignoreIfMissing) (.load)))
@@ -20,11 +23,54 @@
     :messages messages}
    (select-keys model [:api-key :api-endpoint])))
 
+(defrecord DiffFile [path text])
+
+(defn- resource->DiffFile [path]
+  (->> path (io/resource) slurp (DiffFile. path)))
+
+(defn- udiff [old-file new-file]
+  (let [{p0 :path s0 :text} old-file
+        {p1 :path s1 :text} new-file
+        [ls0 ls1] (map str/split-lines [s0 s1])
+        diff (. DiffUtils diff ls0 ls1)
+        udiff (. UnifiedDiffUtils generateUnifiedDiff p0 p1 ls0 diff 0)]
+    (str/join "\n" udiff)))
+
+(defn- llm-predict-udiff-raw [llm f0 f1]
+  (let [diff (udiff f0 f1)]
+    (->
+     "Given this initial file `%s` with the following contents:
+
+%s
+
+... and the following unified diff representing a programmer's ongoing changes to that file:
+
+%s
+
+Predict the next change that the programmer will make to the file.
+
+Please note that your answer should also be a unified diff,
+and that the diff should be relative to the initial file, not the file after the programmer's changes."
+     (format (:path f0) (:text f0) diff)
+     (->> (assoc {:role "user"} :content) vector)
+     (->> (llm-respond llm))
+     (-> :choices first :message :content))))
+
+(defn- llm-predict-udiff [& args]
+  (-> (apply llm-predict-udiff-raw args)
+      (str/split #"```.*\n" 2) second (str/split #"```" 2) first))
+
+(defn- apply-udiff [s udiff]
+  (-> udiff
+      str/split-lines
+      (->> (. UnifiedDiffUtils parseUnifiedDiff))
+      (. applyTo (str/split-lines s))
+      (->> (str/join "\n"))))
+
 (defn -main
   [& _args]
-  (println
-   (-> (llm-model-from-env)
-       (llm-respond
-        [{:role "user"
-          :content "The Los Angeles Dodgers won the World Series in 2020. Where was it played?"}]))))
-
+  (let [llm (llm-model-from-env)
+        [f0 f1] (map resource->DiffFile ["before.mbt" "middle.mbt"])]
+    (-> (time (llm-predict-udiff llm f0 f1))
+        (->> (apply-udiff (:text f0)))
+        println)))
