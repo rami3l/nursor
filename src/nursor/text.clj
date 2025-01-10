212f5403b9bc07d4f6b912d58fc248779ec5e815
rami3l <rami3l@outlook.com>
2025-01-10 15:17:55 +0800

    fix(text): fix handling of nested Markdown code blocks

--- src/nursor/text.clj
+++ src/nursor/text.clj
@@ -2,4 +2,8 @@
   (:require [clojure.string :as str]))
 
 (defn extract-code-block [s]
-  (-> s (str/split #"```.*\n" 2) second (str/split #"```" 2) first))
+  (->> s str/split-lines
+       (drop-while #(not (re-matches #"^```.*$" %)))
+       (drop 1)
+       (take-while #(not (re-matches #"^```" %)))
+       (str/join "\n")))
