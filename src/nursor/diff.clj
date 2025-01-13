166de8994173ce261d71e2c96dbe1d4e118ce9fc
rami3l <rami3l@outlook.com>
2025-01-13 14:26:16 +0800

    feat(diff): replace `relocate-hunk-leader` with `.applyFuzzy`

--- src/nursor/diff.clj
+++ src/nursor/diff.clj
@@ -12,36 +12,12 @@
         udiff (UnifiedDiffUtils/generateUnifiedDiff p0 p1 ls0 diff 0)]
     (str/join "\n" udiff)))
 
-(defn- udiff-apply-lns [s-lns udiff-lns]
+(defn- udiff-apply-lns [s-lns udiff-lns max-fuzz]
   (-> udiff-lns
       UnifiedDiffUtils/parseUnifiedDiff
-      (.applyTo s-lns)
+      (.applyFuzzy s-lns max-fuzz)
       (->> (str/join "\n"))))
-
-(defn- udiff-extract-src-hunk-start [hunk-header]
-  (->> hunk-header (re-seq #"^@@[ ]+-(\d+)") first second Integer/parseInt))
-
-(defn diff-ln-matches? [diff-ln src-ln]
-  (-> diff-ln (str/replace-first #"^-?\s+" "") (= (str/triml src-ln))))
-
-(defn- relocate-hunk-leader [ln-pattern base s-lns]
-  (->> (range 1 (count s-lns)) (mapcat #(vector (+ base %1) (- base %1)))
-       (filter #(diff-ln-matches? ln-pattern (get s-lns %)))
-       first))
-
-(defn- udiff-lns-rectify-start [s-lns udiff-lns]
-  (let [hunk-headers (keep-indexed #(when (re-find #"^@@" %2) (vector %1 %2)) udiff-lns)
-        udiff-lns (transient udiff-lns)]
-    (doseq [[i hunk-header] hunk-headers
-            :let [udiff-hunk-leader (get udiff-lns (inc i))
-                  src-hunk-start (udiff-extract-src-hunk-start hunk-header)
-                  src-hunk-start (dec src-hunk-start)]
-            :when (not (diff-ln-matches? udiff-hunk-leader (get s-lns src-hunk-start)))]
-      (let [start (relocate-hunk-leader udiff-hunk-leader src-hunk-start s-lns)]
-        (assoc! udiff-lns i (format "@@ -%d +1 @@" (inc start)))))
-    (persistent! udiff-lns)))
 
 (defn udiff-apply [s udiff & {:keys [strict] :or {strict false}}]
   (let [[s-lns udiff-lns] (map str/split-lines [s udiff])]
-    (udiff-apply-lns s-lns (if strict udiff-lns
-                             (udiff-lns-rectify-start s-lns udiff-lns)))))
+    (udiff-apply-lns s-lns udiff-lns (if strict 0 5))))
